3f0feaa08842090697bdb9cb82ceceb82c3e1464
venkata mutyala <venkata@venkatamutyala.com>
2021-11-13 20:01:11 +0000

    wip

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
 
 # Python program to demonstrate
 # main() function
-  
-  
-print("Hello")
+import boto3
+from botocore.config import Config
+
+
+
   
 # Defining main function
 def main():
     print("hey there")
+    azs = (get_all_regions_and_avaliability_zones().items())
+
   
   
+def get_all_instance_types_by_az(azs):
+    for az in azs:
+        client.get_paginator('describe_instance_type_offerings')
+
+def get_all_regions_and_avaliability_zones():
+    all_zones_to_regions = {}
+    regions = boto3.client('ec2').describe_regions()['Regions']
+    for region in regions:
+        ec2 = boto3.client('ec2', region_name=region['RegionName'])
+        response = ec2.describe_availability_zones()
+        for zone in response['AvailabilityZones']:
+            if zone['ZoneType'] == 'availability-zone':
+                all_zones_to_regions[zone["ZoneName"]] = zone['RegionName']
+                
+    return all_zones_to_regions
+
+        
+    
+
+
+
 # Using the special variable 
 # __name__
 if __name__=="__main__":
     main()
+
